8af7b8c3bd63124da3b66a0830a5f1e5a05730cf
BeegorMif <a.stesmith1@gmail.com>
2014-07-12 18:05:06 +0100

    Except IOError for Speedfan logs

--- sickbeard/sf.py
+++ sickbeard/sf.py
@@ -6,10 +6,14 @@ def getTemps():
 	log_file_date = datetime.date(2011,1,29).today().isoformat().replace('-','')
 	log_files = []
 	log_file = os.path.join( 'W:/', 'SFLog' + log_file_date + '.csv' )
-	with open(log_file, 'rb') as fh:
-		lines = fh.readlines()
-		last = lines[-1]
-	temp_list = last.strip().split('\t')
-	del temp_list[0]
-	temps = temp_list
-	return temps
+	try:
+		with open(log_file, 'rb') as fh:
+			lines = fh.readlines()
+			last = lines[-1]
+			temp_list = last.strip().split('\t')
+			del temp_list[0]
+			temps = temp_list
+			return temps
+	except IOError:
+			temps = "Error"
+			return temps
